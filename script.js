14a6b8398cf229a90602f267b70299461d04aff6
jlmoer <jlmoerdijk@gmail.com>
2022-01-20 10:40:09 +0100

    Suite du projet fullstack

--- script.js
+++ script.js
@@ -10,10 +10,24 @@ function getAllCountries() {
         }
     });
 }
+
 $(() => {
     $("#btnShowData").click(() => {
         getAllCountries();
     })
 })
+
+function getOneCountry() {
+    $.ajax({
+        url: 'https://restcountries.com/v3.1/all',
+        success: function(data, statuts, response) {
+            console.log(data);
+            let dataOneCountry = data.map((country) => {
+                return `<li> ${country.name[0].common} </li>`
+            })
+            $("#countryName").html(dataOneCountry[0].join(""))
+        }
+    })
+}
 
     
