cf1ec9c2920f9d7c109f2e8e48381ce7e6445c24
jlmoer <jlmoerdijk@gmail.com>
2022-01-20 14:40:33 +0100

    Etape 1 terminée

--- script.js
+++ script.js
@@ -13,21 +13,32 @@ function getAllCountries() {
 
 $(() => {
     $("#btnShowData").click(() => {
-        getAllCountries();
+        let inputOneCountry = $("#oneCountry").val();
+        console.log(inputOneCountry);
+        if (inputOneCountry.length === 0) {
+            getAllCountries();
+        } else {
+            getOneCountry(inputOneCountry);
+        }
     })
 })
 
-function getOneCountry() {
+
+
+function getOneCountry(countryName) {
     $.ajax({
-        url: 'https://restcountries.com/v3.1/all',
+        url: `https://restcountries.com/v3.1/name/${countryName}`,
         success: function(data, statuts, response) {
-            console.log(data);
-            let dataOneCountry = data.map((country) => {
-                return `<li> ${country.name[0].common} </li>`
-            })
-            $("#countryName").html(dataOneCountry[0].join(""))
+            console.log("data", data);
+            let dataOneCountry = `<li> ${data[0].name.common} - ${data[0].capital} - ${data[0].region} </li>`
+            $("#countryList").html(dataOneCountry)
         }
     })
 }
 
-    
+$(() => {
+    $("#countryName").click(() => {
+        getOneCountry();
+    })
+})
+
